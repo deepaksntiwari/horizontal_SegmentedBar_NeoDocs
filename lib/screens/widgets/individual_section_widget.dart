a2fbd5d7cebfccf79648dcbfd6422189c831666b
deepaksntiwari <deepaksntiwary@gmail.com>
2024-02-21 00:00:37 +0530

    Added message on screen and few other enhancements

---
+++ lib/screens/widgets/individual_section_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:horizontal_segmentedbar_neodocs/data/models/section_data.dart';
+
+class Section extends StatelessWidget {
+  const Section(
+      {super.key,
+      required this.barData,
+      required this.totalRange,
+      required this.totalWidth});
+  final SectionData barData;
+  final int totalRange;
+  final double totalWidth;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: (barData.end - barData.start) * totalWidth ~/ totalRange,
+      child: Container(
+        height: 20,
+        color: barData.color,
+      ),
+    );
+  }
+}
